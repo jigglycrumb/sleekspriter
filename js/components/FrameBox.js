dcfafb17ea770589a525ec8e09f2d931adb30132
Volker Wieban <thesquidpeople@googlemail.com>
2014-03-20 03:33:22 +0100

    some CSS improvements

--- js/components/FrameBox.js
+++ js/components/FrameBox.js
@@ -1,13 +1,17 @@
 var FrameBox = React.createClass({
   mixins: [FoldableMixin],
   render: function() {
+    var totalFrames = this.props.io.frames.x * this.props.io.frames.y;
     return (
       <div id="FrameBox" className="box">
         <h4 className="foldable-handle">Frames</h4>
         <div className="foldable-fold">
           <div></div>
           <div className="actions">
-
+            Frame
+            <input type="number" defaultValue={this.props.editor.frame} />
+            of
+            {totalFrames}
           </div>
         </div>
       </div>
