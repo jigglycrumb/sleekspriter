f5d17498ce527ef609586a5139a616974b574430
Volker Wieban <thesquidpeople@googlemail.com>
2014-07-27 00:33:36 +0200

    frame canvases now react to move tool

--- dist/pixler.js
+++ dist/pixler.js
@@ -26352,14 +26352,14 @@ var FrameCanvasMixin = {
 
       switch(this.state.topic) {
         case 'stage.pixel.fill':
-          var pixelsAbove = this.getPixelsAbove(x, y, z);
+          var pixelsAbove = this.getPixelsAbove(editor.pixels.frame, x, y, z);
           if(pixelsAbove === false) Pixel.paint(canvas, x, y, this.state.data.color);
           break;
 
         case 'stage.pixel.clear':
-          var pixelsAbove = this.getPixelsAbove(x, y, z);
+          var pixelsAbove = this.getPixelsAbove(editor.pixels.frame, x, y, z);
           if(pixelsAbove === false) {
-            var pixelBelow = this.getPixelBelow(x, y, z);
+            var pixelBelow = this.getPixelBelow(editor.pixels.frame, x, y, z);
             if(pixelBelow === false) Pixel.clear(canvas, x, y);
             else Pixel.paint(canvas, x, y, pixelBelow.toHex());
           }
@@ -26368,21 +26368,25 @@ var FrameCanvasMixin = {
         case 'app.frame.select':
           this.paintFrame();
           break;
+
+        case 'canvas.update':
+          this.previewFrame();
+          break;
       }
 
       this.resetState();
     }
   },
-  getPixelsAbove: function(x, y, z) {
-    var above = editor.pixels.frame.filter(function(px) {
+  getPixelsAbove: function(pixels, x, y, z) {
+    var above = pixels.filter(function(px) {
       return px.frame == this.props.id && px.x == x && px.y == y && px.z > z;
     }, this);
 
     if(above.length == 0) return false;
     return above;
   },
-  getPixelBelow: function(x, y, z) {
-    var below = editor.pixels.frame.filter(function(px) {
+  getPixelBelow: function(pixels, x, y, z) {
+    var below = pixels.filter(function(px) {
       return px.frame == this.props.id && px.x == x && px.y == y && px.z < z;
     }, this);
 
@@ -26400,9 +26404,24 @@ var FrameCanvasMixin = {
 
     pixels.forEach(function(px) {
       if(px.frame === this.props.id) {
-        var pixelsAbove = this.getPixelsAbove(px.x, px.y, px.z);
+        var pixelsAbove = this.getPixelsAbove(editor.pixels.frame, px.x, px.y, px.z);
         if(pixelsAbove === false) Pixel.paint(canvas, px.x, px.y, px.toHex());
       }
+    }, this);
+  },
+  previewFrame: function() {
+    var canvas = this.getDOMNode(),
+        pixels = editor.pixels.preview.slice(0); // clone preview pixels
+
+    editor.pixels.frame.forEach(function(px) {
+      if(px.layer !== this.state.data.layer) pixels.push(px);
+    }, this);
+
+    canvas.width = canvas.width;
+
+    pixels.forEach(function(px) {
+      var pixelsAbove = this.getPixelsAbove(pixels, px.x, px.y, px.z);
+      if(pixelsAbove === false) Pixel.paint(canvas, px.x, px.y, px.toHex());
     }, this);
   },
 };
