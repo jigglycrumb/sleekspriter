2224ea24e02257f2db41fdd9776d90f0304134a7
hpcodecraft <thesquidpeople@googlemail.com>
2017-08-03 23:47:42 +0200

    Adds test for Pixel class

---
+++ tests/classes/Pixel.test.js
@@ -0,0 +1,15 @@
+import Pixel from "classes/Pixel";
+
+describe("Pixel", () => {
+  it("is a Pixel with frame, layer, x and y coordinates, rgb and alpha values", () => {
+    const pixel = new Pixel(23, 42, 2, 3, 255, 200, 128, 1);
+    expect(pixel.frame).toBe(23);
+    expect(pixel.layer).toBe(42);
+    expect(pixel.x).toBe(2);
+    expect(pixel.y).toBe(3);
+    expect(pixel.r).toBe(255);
+    expect(pixel.g).toBe(200);
+    expect(pixel.b).toBe(128);
+    expect(pixel.a).toBe(1);
+  });
+});
