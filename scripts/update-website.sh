16a37bc94791069fa44d228e6f21cd7853e9da2b
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2020-08-21 22:38:21 +0200

    feat: automated website update (i hope)

--- scripts/update-website.sh
+++ scripts/update-website.sh
@@ -6,12 +6,10 @@ WD=$(pwd)
 
 PRODUCT_VERSION="$(echo $GITHUB_REF | cut -d'/' -f3)"
 
-# PRODUCT_VERSION="$(cut -d'/' -f3 <<<"$GITHUB_REF")"
-
 echo "Updating website for SleekSpriter $PRODUCT_VERSION"
 
-# cp -R $WD/website/* $WD/docs
+cp -R $WD/website/* $WD/docs
 
 # TODO fallback to currently set version (read from docs or so) and set it
 
-# sed "s/%VERSION%/$PRODUCT_VERSION/g" website/index.html >docs/index.html
+sed "s/%VERSION%/$PRODUCT_VERSION/g" website/index.html >docs/index.html
