23d91fa1f5e41e0cb9e8bb013f4960a563918ede
hpcodecraft <thesquidpeople@gmail.com>
2020-08-21 17:28:55 +0200

    add website source

---
+++ scripts/update-website.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+WD=$(pwd)
+PRODUCT_VERSION=$GITHUB_REF
+
+echo "Updating website for SleekSpriter $PRODUCT_VERSION"
+
+# cp -R $WD/website/* $WD/docs
+
+# TODO fallback to currently set version (read from docs or so) and set it
+
+# sed "s/%VERSION%/$PRODUCT_VERSION/g" website/index.html >docs/index.html
